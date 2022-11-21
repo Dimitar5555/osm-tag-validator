bd6a4c39d8c9aac223ae6b07eaaffbd1999e55a4
Sky <19364673+Dimitar5555@users.noreply.github.com>
2022-11-21 17:39:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const base_url = 'https://taginfo.openstreetmap.org/api/4/key/values?';
+var tags = JSON.parse(fs.readFileSync('tags.json'));
+function get_stats(key, page){
+    return fetch(base_url + `key=${key}&filter=all&lang=en&sortname=count&sortorder=desc&rp=800&qtype=&format=json_pretty&page=${page}`);
+}
+function check_value(value, allowed_values, non_standard_usages){
+    if(allowed_values.indexOf(value)==-1){
+        if(non_standard_usages[value]){
+            non_standard_usages[value]++;
+        }
+        else{
+            non_standard_usages[value] = 1;
+        }
+    }
+}
+function process_value(value, allowed_values, non_standard_usages, options={colon_split: false, semicolon_split: false}){
+    if(!options.colon_split && !options.semicolon_split){
+        check_value(value, allowed_values, non_standard_usages);
+        return;
+    }
+
+    var values;
+    if(options.colon_split){
+        values = value.split('|');
+    }
+    else if(options.semicolon_split){
+        values = value.split(';');
+    }
+    values.forEach(value => {
+        if(options.colon_split && options.semicolon_split){
+            value.split(';').forEach(part => {
+                check_value(part, allowed_values, non_standard_usages);
+            })
+        }
+        else{
+            check_value(value, allowed_values, non_standard_usages);
+        }
+    })
+}
+
+function process_page(key, page, allowed_values, non_standard_usages, options){
+    get_stats(key, page)
+    .then(res => res.json())
+    .then(res => {
+        console.log(`Processing page ${res.page} of ${Math.ceil(res.total/res.rp)} pages for ${key}`);
+        res.data.forEach(element => {
+            process_value(element.value, allowed_values, non_standard_usages, options);
+        });
+        if(Math.ceil(res.total/res.rp)>res.page){
+            process_page(key, page+1, allowed_values, non_standard_usages, options);
+        }
+        else{
+            fs.writeFileSync(`website/data/${key.replaceAll(':', '_')}.json`, JSON.stringify(non_standard_usages));
+        }
+    })
+    .catch(error => {throw new Error(error);});
+}
+
+if (!fs.existsSync('website/data/')){
+    fs.mkdirSync('website/data/', {recursive: true});
+}
+fs.writeFileSync('website/data/tags.json', JSON.stringify(tags));
+
+tags.forEach(tag => {
+    if(tag.inherit_from){
+        var source = tags.filter(a => a.tag==tag.inherit_from)[0];
+        tag.allowed_values = source.allowed_values;
+        tag.colon_split = source.colon_split;
+        tag.semicolon_split = source.semicolon_split;
+    }
+    process_page(tag.tag, 1, tag.allowed_values, {}, {colon_split: tag.colon_split, semicolon_split: tag.semicolon_split});
+});
---
+++ website/web_app.js
@@ -0,0 +1,82 @@
+var data = {};
+function create_issues_table(key){
+    var issues = data[key.replaceAll(':', '_')];
+    var table = document.querySelector('#issues_table');
+    var old_tbody = table.querySelector('tbody');
+    var new_tbody = document.createElement('tbody');
+    var sorted_issues = Object.keys(issues).map((key) => [key, issues[key]]);
+    sorted_issues.sort((a, b) => a[1]<b[1]);
+    console.log(issues, sorted_issues);
+    sorted_issues.forEach((issue, index) => {
+        var tr = document.createElement('tr');
+
+        var td1 = document.createElement('td');
+        td1.innerHTML = index+1;
+        var td2 = document.createElement('td');
+        td2.innerHTML = issue[0];
+        var td3 = document.createElement('td');
+        td3.innerHTML = issue[1];
+
+        var td4 = document.createElement('td');
+        var a1 = document.createElement('a');
+        a1.innerText = 'Overpass Turbo';
+        a1.href = `https://overpass-turbo.eu/?w="${key}"${issue.exact_match?'=':'~'}"${issue[0]}"+global`;
+        a1.target = '_blank';
+        var query = `[out:xml][timeout:25];(nwr["${key}"${issue.exact_match?'=':'~'}"${issue[0]}"];);out meta;>;out meta qt;`;
+        var a2 = document.createElement('a');
+        a2.innerText = 'Level0';
+        a2.href = `https://level0.osmz.ru/?url=://overpass-api.de/api/interpreter?data=${encodeURI(encodeURI(query))}`;
+        http://level0.osmz.ru/?url=%2F%2Foverpass-api.de%2Fapi%2Finterpreter%3Fdata
+        a2.target = '_blank';
+        td4.appendChild(a1);
+        td4.appendChild(a2);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        new_tbody.appendChild(tr);
+    });
+    old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
+}
+function get_data(){
+    fetch('tags.json')
+    .then(res => res.json())
+    .then(res => {
+        var table = document.createElement('table');
+        res.forEach(tag => {
+            fetch(`results/${tag.tag.replaceAll(':', '_')}.json`)
+            .then(res => res.json())
+            .then(res => {
+                data[tag.tag.replaceAll(':', '_')] = res;
+            });
+            var tr = document.createElement('tr');
+            var td1 = document.createElement('td');
+            var a = document.createElement('a');
+            a.href = `?tag=${tag.tag}`;
+            a.setAttribute('data-tag', tag.tag);
+            a.innerHTML = tag.tag;
+            td1.appendChild(a);
+            tr.appendChild(td1);
+            table.appendChild(tr);
+        });
+        document.body.appendChild(table);
+        
+        var table2 = document.createElement('table');
+        table2.id = 'issues_table';
+        table2.appendChild(document.createElement('thead'));
+        table2.appendChild(document.createElement('tbody'));
+
+        document.body.appendChild(table2);
+        Array.from(document.querySelectorAll('a'))
+        .forEach(element => {
+            element.addEventListener('click', (e) => {
+                e.preventDefault();
+                var key = e.target.dataset.tag;
+                window.history.pushState(`object or string`, document.title, `?key=${key}`);
+                create_issues_table(key)
+                console.log(e);
+            })
+        })
+    });
+}
+get_data();
