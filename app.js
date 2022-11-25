a2b316cad80615b8e814117199ed9bb56c76cf96
Sky <19364673+Dimitar5555@users.noreply.github.com>
2022-11-25 17:24:14 +0200

    Refactor code and add new keys

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
-const base_url = 'https://taginfo.openstreetmap.org/api/4/key/values?';
-var tags = JSON.parse(fs.readFileSync('tags.json'));
-function get_stats(key, page){
-    return fetch(base_url + `key=${key}&filter=all&lang=en&sortname=count&sortorder=desc&rp=800&qtype=&format=json_pretty&page=${page}`);
+const base_url = 'https://taginfo.openstreetmap.org/api/4/key/';
+const VALUES_PER_PAGE = 800;
+var keys = JSON.parse(fs.readFileSync('keys.json'));
+async function get_stats(key){
+    console.log
+    return fetch(base_url + `stats?key=${key}`);
+}
+async function get_values(key, page){
+    return fetch(base_url + `values?key=${key}&filter=all&lang=en&sortname=count&sortorder=desc&rp=${VALUES_PER_PAGE}&qtype=&format=json_pretty&page=${page}`);
 }
 function check_value(value, allowed_values, non_standard_usages){
     if(allowed_values.indexOf(value)==-1){
@@ -40,38 +45,67 @@ function process_value(value, allowed_values, non_standard_usages, options={colo
     })
 }
 
-function process_page(key, page, allowed_values, non_standard_usages, options){
-    get_stats(key, page)
+async function process_page(key, page, allowed_values, non_standard_usages, options){
+    return get_values(key, page)
     .then(res => res.json())
     .then(res => {
         console.log(`Processing page ${res.page} of ${Math.ceil(res.total/res.rp)} pages for ${key}`);
         res.data.forEach(element => {
             process_value(element.value, allowed_values, non_standard_usages, options);
         });
-        if(Math.ceil(res.total/res.rp)>res.page){
-            process_page(key, page+1, allowed_values, non_standard_usages, options);
+    });
+}
+function generate_keys_data(){
+    var keys_copy = JSON.parse(JSON.stringify(keys));
+    var res = [];
+
+    keys_copy.sort((a, b) => a.inherit_from!==undefined && a.parent_key==undefined);
+    keys_copy.forEach(item => {
+        if(item.inherit_from==undefined && item.parent_key==undefined){
+            //main tag
+            item.subkeys = [];
+            res.push(item);
         }
         else{
-            fs.writeFileSync(`docs/data/${key.replaceAll(':', '_')}.json`, JSON.stringify(non_standard_usages));
+            //subkey
+            res[res.findIndex(main_item => main_item.key==(item.parent_key || item.inherit_from))].subkeys.push(item.key);
         }
-    })
-    .catch(error => {throw new Error(error);});
+    });
+    res.sort((a, b) => a.key < b.key);
+    // res.forEach(key => {
+    //     if(key.subkeys){
+    //         key.subkeys.sort();
+    //     }
+    // });
+    return res;
 }
-
-if (!fs.existsSync('docs/data/')){
-    fs.mkdirSync('docs/data/', {recursive: true});
-}
-var tags_copy = JSON.parse(JSON.stringify(tags));
-tags_copy.forEach(tag => delete tag.allowed_values)
-fs.writeFileSync('docs/data/tags.json', JSON.stringify(tags_copy));
-delete tags_copy;
-
-tags.forEach(tag => {
-    if(tag.inherit_from){
-        var source = tags.filter(a => a.tag==tag.inherit_from)[0];
-        tag.allowed_values = source.allowed_values;
-        tag.colon_split = source.colon_split;
-        tag.semicolon_split = source.semicolon_split;
+fs.writeFileSync('docs/keys.json', JSON.stringify(generate_keys_data()));
+var detected_issues = {};
+var promises = [];
+keys.forEach(async key => {
+    if(key.skip_validation){
+        return;
     }
-    process_page(tag.tag, 1, tag.allowed_values, {}, {colon_split: tag.colon_split, semicolon_split: tag.semicolon_split});
+    if(key.inherit_from){
+        var source = keys.filter(a => a.key==key.inherit_from)[0];
+        key.allowed_values = source.allowed_values;
+        key.colon_split = source.colon_split;
+        key.semicolon_split = source.semicolon_split;
+    }
+    promises.push(get_stats(key.key)
+    .then(res => res.json())
+    .then(stats => {
+        var total_pages = Math.ceil(stats.data.find(a => a.type=='all').values / VALUES_PER_PAGE);
+        detected_issues[key.key] = {};
+        for(let i1=1;i1<=total_pages;i1++){
+            promises.push(process_page(key.key, i1, key.allowed_values, detected_issues[key.key], {colon_split: key.colon_split, semicolon_split: key.semicolon_split}));
+        }
+    }));
 });
+setTimeout(() => {
+    Promise.all(promises)
+    .then(() => {
+        console.log('Wrote data to docs/detected_issues.json');
+        fs.writeFileSync('docs/detected_issues.json', JSON.stringify(detected_issues));
+    });
+}, 2000);
--- docs/web_app.js
+++ docs/web_app.js
@@ -1,30 +1,42 @@
 var data = {};
 var base_data = {};
+function create_html_element(element, attributes={}){
+    var el = document.createElement(element);
+    if(Object.keys(attributes).length>0){
+        for(attribute in attributes){
+            if(attribute=='innerText'){
+                el.innerText = attributes[attribute];
+                continue;
+            }
+            el.setAttribute(attribute, attributes[attribute]);
+        }
+    }
+    return el;
+}
 function create_issues_table(key){
-    var issues = data[key.replaceAll(':', '_')];
-    var key_data = base_data[key.replaceAll(':', '_')];
+    var issues = data[key];
+    var key_data = base_data[key];
     var old_tbody = document.querySelector('#issues_table').querySelector('tbody');
-    var new_tbody = document.createElement('tbody');
-    var sorted_issues = Object.keys(issues).map((key) => [key, issues[key]]);
+    var new_tbody = create_html_element('tbody');
+    var sorted_issues = Object.keys(issues).map(value => [value, data[key][value]]);
     sorted_issues.sort((a, b) => a[1]<b[1]);
     sorted_issues.forEach((issue) => {
-        var tr = document.createElement('tr');
+        var tr = create_html_element('tr');
 
-        var td1 = document.createElement('td');
-        td1.innerText = `${key}${key_data.fuzzy_match?'~':'='}${issue[0]}`;
-        var td2 = document.createElement('td');
-        td2.innerText = issue[1];
+        var td1 = create_html_element('td', {innerText: `${key}${key_data.fuzzy_match?'~':'='}${issue[0]}`});
+        var td2 = create_html_element('td', {innerText: issue[1]});
 
-        var td3 = document.createElement('td');
-        var a1 = document.createElement('a');
-        a1.innerText = 'Overpass Turbo';
-        a1.href = `https://overpass-turbo.eu/?w="${key}"${key_data.fuzzy_match?'~':'='}"${issue[0]}"+global&R`;
-        a1.target = '_blank';
+        var td3 = create_html_element('td');
+        var a1 = create_html_element('a', {
+            innerText: 'Overpass Turbo',
+            href: `https://overpass-turbo.eu/?w="${key}"${key_data.fuzzy_match?'~':'='}"${issue[0]}"+global&R`,
+            target: '_blank'});
         var query = `[out:xml][timeout:25];(nwr["${key}"${key_data.fuzzy_match?'~':'='}"${issue[0]}"];);out meta;>;out meta qt;`;
-        var a2 = document.createElement('a');
-        a2.innerText = 'Level0';
-        a2.href = `https://level0.osmz.ru/?url=${encodeURI('//overpass-api.de/api/interpreter?data='+encodeURI(query))}`;
-        a2.target = '_blank';
+        var a2 = create_html_element('a', {
+            innerText: 'Level0',
+            href: `https://level0.osmz.ru/?url=${encodeURI('//overpass-api.de/api/interpreter?data='+encodeURI(query))}`,
+            target: '_blank'
+        });
         td3.appendChild(a1);
         td3.appendChild(document.createTextNode(' '))
         td3.appendChild(a2);
@@ -33,87 +45,88 @@ function create_issues_table(key){
         tr.appendChild(td3);
         new_tbody.appendChild(tr);
     });
-    old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
+    if(sorted_issues.length==0){
+        var tr = create_html_element('tr');
+        var td = create_html_element('td', {innerText: `There are now elements with improper values for ${key}!`, colspan: 3, class: 'text-center'});
+        tr.appendChild(td);
+        new_tbody.appendChild(tr);
+    }
+    old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
 }
-function get_data(){
-    fetch('data/tags.json')
+function load_data(){
+    var promises = [];
+    fetch('keys.json')
     .then(res => res.json())
-    .then(res => res.sort((a, b) => a.inherit_from > b.inherit_from || a.parent_tag > b.parent_tag))
-    .then(loc_data => {
-        var old_tbody1 = document.querySelector('#tags_list').querySelector('tbody');
-        var new_tbody1 = document.createElement('tbody');
-        var last_parent_tag;
-        loc_data.forEach(tag => {
-            fetch(`data/${tag.tag.replaceAll(':', '_')}.json`)
-            .then(res => res.json())
-            .then(res => {
-                var tr = document.createElement('tr');
-                var td0 = document.createElement('td');
-                var td1 = document.createElement('td');
-                if(tag.inherit_from){
-                    var source = data[tag.inherit_from.replaceAll(':', '_')];
-                    tag.allowed_values = source.allowed_values;
-                    tag.colon_split = source.colon_split;
-                    tag.semicolon_split = source.semicolon_split;
-                    tag.fuzzy_match = source.fuzzy_match;
-                }
-                if(tag.inherit_from || tag.parent_tag){
-                    tr.setAttribute('data-parent-tag', last_parent_tag);
-                    tr.classList.add('d-none');
-                }
-                else{
-                    last_parent_tag = tag.tag;
-                    if(loc_data.filter(a => a.inherit_from==last_parent_tag || a.parent_tag==last_parent_tag).length>0){
-                        var btn = document.createElement('button');
-                        btn.innerText = '+';
-                        btn.setAttribute('data-expand-parent-tag', last_parent_tag);
-                        btn.onclick = (e) => {
-                            e.preventDefault();
-                            Array.from(document.querySelectorAll(`[data-parent-tag='${e.target.dataset.expandParentTag}']`))
-                            .forEach(element => {
-                                element.classList.toggle('d-none')
-                            });
-                            btn.innerText = btn.innerText=='-'?'+':'-'; 
-                        };
-                        td0.appendChild(btn);
-                        td0.classList.add('text-center');
-                    }
-                }
-                base_data[tag.tag.replaceAll(':', '_')] = tag;
-                data[tag.tag.replaceAll(':', '_')] = res;
-
-                var a = document.createElement('a');
-                a.href = `?tag=${tag.tag}`;
-                a.setAttribute('data-tag', tag.tag);
-                a.innerText = tag.tag;
-                a.onclick = (e) => {
-                    e.preventDefault();
-                    var key = e.target.dataset.tag;
-                    window.history.pushState(`object or string`, document.title, `?key=${key}`);
-                    create_issues_table(key);
-                };
-                td1.appendChild(a);
-                var td2 = document.createElement('td');
-                td2.innerText = Object.keys(res).reduce((total, tag) => total + res[tag], 0);
-                tr.appendChild(td0);
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                new_tbody1.appendChild(tr);
-            });
-        });
-        old_tbody1.parentNode.replaceChild(new_tbody1, old_tbody1);
-    })
     .then(res => {
-        Array.from(document.querySelectorAll('a'))
-        .forEach(element => {
-            console.log(element);
-            element.addEventListener('click', (e) => {
-                e.preventDefault();
-                var key = e.target.dataset.tag;
-                window.history.pushState(`object or string`, document.title, `?key=${key}`);
-                create_issues_table(key);
-            })
-        })
+        res.forEach(key => {
+            base_data[key.key] = key;
+            
+            if(key.subkeys){
+                key.subkeys.forEach(subkey => {
+                    var subkey_data = {};
+                    subkey_data.fuzzy_match = key.fuzzy_match;
+                    subkey_data.parent_key = key.key;
+                    base_data[subkey] = subkey_data;
+                });
+            }
+        });
+        return fetch('detected_issues.json');
+    })
+    .then(res => res.json())
+    .then(res => data = res)
+    .then(() => {
+        console.log(data);
+        Promise.all(promises)
+        .then(() => {
+            generate_key_list_table();
+        });
     });
 }
-get_data();
+function generate_row(key){
+    var tr = create_html_element('tr');
+    var td0 = create_html_element('td', {class: 'text-center'});
+    if(base_data[key].subkeys && base_data[key].subkeys.length>0){
+        var btn = create_html_element('button', {innerText: '+', 'data-expand-parent-key': key});
+        btn.onclick = (e) => {
+            e.preventDefault();
+            Array.from(document.querySelectorAll(`[data-parent-key='${e.target.dataset.expandParentKey}']`))
+            .forEach(element => {
+                element.classList.toggle('d-none')
+            });
+            btn.innerText = btn.innerText=='-'?'+':'-'; 
+        };
+        td0.appendChild(btn);
+    }
+    else{
+        tr.classList.add('d-none');
+        tr.setAttribute('data-parent-key', base_data[key].parent_key);
+    }
+    var td1 = create_html_element('td');
+    var a = create_html_element('a', {href: `?key=${key}`, 'data-key': key, innerText: key});
+    a.onclick = (e) => {
+        e.preventDefault();
+        var key = e.target.dataset.key;
+        window.history.pushState(`object or string`, document.title, `?key=${key}`);
+        create_issues_table(key);
+    };
+    td1.appendChild(a);
+    var total_entries = base_data[key].parent_key?'ffdf':Object.keys(data[key]).reduce((total, value) => total + data[key][value], 0);
+    var td2 = create_html_element('td', {innerText: total_entries});
+
+    tr.appendChild(td0);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    return tr;
+}
+function generate_key_list_table(){
+    var old_tbody = document.querySelector('#tags_list').querySelector('tbody');
+    var new_tbody = create_html_element('tbody');
+    Object.keys(base_data).forEach(key => {
+        if(base_data[key].skip_validation){
+            return;
+        }
+        new_tbody.appendChild(generate_row(key));
+    });
+    old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+}
+load_data();
